8dcdc03f45a7c5fe61e8ae7dedaa39276937bd7b
Nisrine2001 <nisrine.touijer@gmail.com>
2023-11-28 15:35:23 -0800

    Task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,4 +3,4 @@ for num in range(10):
     for digit in range(num, 10):
         if num < digit:
             print("{:d}{:d}".format(num, digit),
-                    end='\n' if num == 8 and digit == 9 else ", ")
+                  end='\n' if num == 8 and digit == 9 else ", ")
